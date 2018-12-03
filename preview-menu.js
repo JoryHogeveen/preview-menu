d099fb30d706e2a9a418ff3ec159c979d0adec51
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2018-12-03 13:50:48 +0100

    v0.2 - Store preview menu

---
+++ preview-menu.js
@@ -0,0 +1,96 @@
+/* eslint-disable no-extra-semi */
+;/**
+ * Preview Menu
+ * https://wordpress.org/plugins/preview-menu/
+ *
+ * @author  Jory Hogeveen <info@keraweb.nl>
+ * @package Preview_Menu
+ * @since   0.2.0
+ * @version 0.2.0
+ * @preserve
+ *
+ * @var PreviewMenu
+ */
+/* eslint-enable no-extra-semi */
+
+( function( $ ) {
+
+	PreviewMenu.init = function() {
+
+		$('#preview_menu_btn').on( 'click', function( e ) {
+			e.preventDefault();
+			e.stopPropagation();
+
+			var $this      = $( this ),
+				url        = $( '#preview_menu_url' ).val(),
+				menu       = $( '#update-nav-menu input#menu' ).val(),
+				menu_items = $( '#update-nav-menu .menu input, #update-nav-menu .menu select, #update-nav-menu .menu textarea' ).serializeObject(),
+				location   = $( '#preview_menu_location' ).val();
+
+			$( '#preview_menu_btn + .spinner' ).addClass( 'is-active' );
+
+			$.post(
+				ajaxurl,
+				{
+					action: 'preview_menu',
+					_nonce: PreviewMenu._nonce,
+					preview_menu: {
+						menu: menu,
+						items: JSON.stringify( menu_items )
+					}
+				},
+				function( response ) {
+					var success = ( response.hasOwnProperty( 'success' ) && true === response.success ),
+						data    = response.hasOwnProperty( 'data' ) ? response.data : false,
+						menu    = data.hasOwnProperty( 'menu' ) ? response.menu : false;
+
+					if ( success ) {
+
+						url = PreviewMenu.add_query_arg( 'preview_menu', 1, url );
+
+						if ( menu ) {
+							url = PreviewMenu.add_query_arg( 'preview_menu_id', menu, url );
+						}
+						if ( location ) {
+							url = PreviewMenu.add_query_arg( 'preview_menu_location', location, url );
+						}
+
+
+						$( '#preview_menu_btn + .spinner' ).removeClass( 'is-active' );
+						window.open( url, '_blank' );
+					}
+				}
+			);
+		} );
+	};
+
+	PreviewMenu.add_query_arg = function( key, value, url ) {
+		key   = encodeURI( key );
+		value = encodeURI( value );
+
+		url = url.split( '&' );//url.substr(1).split('&');
+
+		var i = url.length;
+		var x;
+		while ( i-- ) {
+			x = url[ i ].split( '=' );
+
+			if ( x[0] === key ) {
+				x[1]     = value;
+				url[ i ] = x.join( '=' );
+				break;
+			}
+		}
+
+		if ( i < 0 ) {
+			url[ url.length ] = [ key, value ].join( '=' );
+		}
+
+		return url.join( '&' );
+	};
+
+	if ( 'undefined' !== typeof PreviewMenu ) {
+		PreviewMenu.init();
+	}
+
+} ( jQuery ) );
--- preview-menu.php
+++ preview-menu.php
@@ -3,7 +3,7 @@
  * @author  Jory Hogeveen <info@keraweb.nl>
  * @package Preview_Menu
  * @since   0.1
- * @version 0.1.1
+ * @version 0.2
  * @licence GPL-2.0+
  * @link    https://github.com/JoryHogeveen/preview-menu/
  *
@@ -11,7 +11,7 @@
  * Plugin Name:       Preview Menu
  * Plugin URI:        https://github.com/JoryHogeveen/preview-menu
  * Description:       Preview menu's on selected locations.
- * Version:           0.1.1
+ * Version:           0.2
  * Author:            Jory Hogeveen
  * Author URI:        https://www.keraweb.nl
  * Text Domain:       preview-menu
@@ -54,11 +54,39 @@ class Preview_Menu
 	private $menu_locations = array();
 
 	/**
+	 * The default menu location.
+	 * @var    string
+	 * @since  0.2
+	 */
+	private $default_location = 'primary';
+
+	/**
 	 * The capability required to access this plugin.
 	 * @var    string
 	 * @since  0.1.1
 	 */
 	private $capability = '';
+
+	/**
+	 * Preview menu value.
+	 * @var    mixed
+	 * @since  0.2
+	 */
+	private $preview_menu = null;
+
+	/**
+	 * Preview menu db option.
+	 * @var    string
+	 * @since  0.2
+	 */
+	private $option = 'preview_menu';
+
+	/**
+	 * Nonce.
+	 * @var    string
+	 * @since  0.2
+	 */
+	private $nonce = 'preview_menu';
 
 	/**
 	 * The single instance of the class.
@@ -139,6 +167,8 @@ class Preview_Menu
 
 		if ( apply_filters( 'preview_menu_enable_meta_box', true ) ) {
 			$this->add_meta_box();
+			add_filter( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ) );
+			add_action( 'wp_ajax_preview_menu', array( $this, 'ajax' ) );
 		}
 	}
 
@@ -147,12 +177,20 @@ class Preview_Menu
 	 * @since  0.1
 	 */
 	public function front() {
+		if ( ! empty( $_GET['preview_menu'] ) ) {
+			$this->preview_menu = $_GET['preview_menu'];
+		}
 
 		/**
 		 * Option to switch to another menu preview for users who can manage menu's.
 		 */
-		if ( ! empty( $_GET['preview_menu'] ) && current_user_can( $this->capability ) ) {
-			add_filter( 'wp_nav_menu_args', array( $this, 'filter_wp_nav_menu_args' ), 1 );
+		if ( $this->preview_menu ) {
+			if ( is_numeric( $this->preview_menu ) && 1 === (int) $this->preview_menu ) {
+				$this->preview_menu = get_option( $this->option );
+				add_filter( 'wp_get_nav_menu_items', array( $this, 'filter_front_wp_get_nav_menu_items' ), 1, 3 );
+			} else {
+				add_filter( 'wp_nav_menu_args', array( $this, 'filter_front_wp_nav_menu_args' ), 1 );
+			}
 		}
 	}
 
@@ -162,10 +200,9 @@ class Preview_Menu
 	 * @param  array  $args
 	 * @return array
 	 */
-	public function filter_wp_nav_menu_args( $args ) {
+	public function filter_front_wp_nav_menu_args( $args ) {
 
-		// @todo Improve default location.
-		$location = apply_filters( 'preview_menu_default_location', 'primary', $args );
+		$location = apply_filters( 'preview_menu_default_location', $this->default_location, $args );
 
 		if ( ! empty( $_GET['preview_menu_location'] ) ) {
 			$location = $_GET['preview_menu_location'];
@@ -175,10 +212,216 @@ class Preview_Menu
 			return $args;
 		}
 
-		$menu = $_GET['preview_menu'];
-		$args['menu'] = $menu;
+		$args['menu'] = $this->preview_menu;
 
 		return $args;
+	}
+
+	/**
+	 * Change menu items.
+	 * @since  0.2
+	 * @param  object[] $items
+	 * @param  object   $menu
+	 * @param  array    $args
+	 * @return object[] mixed
+	 */
+	public function filter_front_wp_get_nav_menu_items( $items, $menu, $args ) {
+		$args = wp_parse_args( $args, array( 'theme_location' => '' ) );
+
+		$location = apply_filters( 'preview_menu_default_location', '', $args );
+
+		if ( ! empty( $_GET['preview_menu_location'] ) ) {
+			$location = $_GET['preview_menu_location'];
+		}
+
+		// Replace menu based on location.
+		if ( $location && $location === $args['theme_location'] ) {
+			return $this->get_preview_menu_items( $args );
+		}
+		// No location given, replace the menu on it's current location.
+		if (
+			! empty( $menu->term_id )
+			&& ! empty( $this->preview_menu['menu'] )
+			&& (int) $menu->term_id === (int) $this->preview_menu['menu']
+		) {
+			return $this->get_preview_menu_items( $args );
+		}
+
+		return $items;
+	}
+
+	/**
+	 * Get menu items.
+	 * @since  0.2
+	 * @param  array    $args
+	 * @return object[]|array[] mixed
+	 */
+	public function get_preview_menu_items( $args ) {
+		static $items;
+
+		if ( ! $items ) {
+
+			if ( empty( $this->preview_menu['items'] ) ) {
+				return array();
+			}
+
+			foreach ( $this->preview_menu['items'] as $key => $item ) {
+				foreach ( $item as $meta => $value ) {
+					$underscore_meta = str_replace( '-', '_', $meta );
+					$private_meta    = '_' . $underscore_meta;
+
+					// Store metadata alias.
+					$item[ $underscore_meta ] = $value;
+					$item[ $private_meta ]    = $value;
+				}
+				$item['ID']               = $key;
+				$item['post_type']        = 'nav_menu_item';
+				$item['post_title']       = $item['menu_item_title'];
+				$item['menu_item_parent'] = $item['menu_item_parent_id'];
+				$item['object_id']        = $item['menu_item_object_id'];
+				$item['object']           = $item['menu_item_object'];
+				$item['type']             = $item['menu_item_type'];
+
+				$this->preview_menu['items'][ $key ] = new WP_Post( (object) $item );
+				$this->preview_menu['items'][ $key ] = wp_setup_nav_menu_item( $this->preview_menu['items'][ $key ] );
+			}
+
+			$items = $this->preview_menu['items'];
+		}
+
+		// @see wp_get_nav_menu_items.
+		if ( ARRAY_A === $args['output'] ) {
+			$items = wp_list_sort( $items, array(
+				$args['output_key'] => 'ASC',
+			) );
+
+			$i = 1;
+			foreach ( $items as $k => $item ) {
+				$items[ $k ]->{ $args['output_key'] } = $i++;
+			}
+		}
+
+		return $items;
+	}
+
+	/**
+	 * @param  null    $null
+	 * @param  int     $id
+	 * @param  string  $key
+	 * @return mixed
+	 */
+	public function filter_front_get_nav_metadata( $null, $id, $key ) {
+		if ( ! empty( $this->preview_menu['items'][ $id ]->{ $key } ) ) {
+			return $this->preview_menu['items'][ $id ]->{ $key };
+		}
+		return $null;
+	}
+
+	/**
+	 * Enqueue scripts.
+	 * @since  0.2
+	 */
+	public function admin_enqueue_scripts() {
+		$screen = get_current_screen();
+		if ( empty( $screen ) ) {
+			return;
+		}
+
+		$version = defined( 'WP_DEBUG' ) && WP_DEBUG ? '0.2' : time();
+		$suffix  = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
+
+		if ( 'nav-menus' === $screen->base ) {
+			wp_enqueue_script(
+				'preview-menu',
+				plugins_url( '/preview-menu' . $suffix . '.js', __FILE__ ),
+				array( 'jquery', 'jquery-serialize-object' ),
+				$version,
+				true
+			);
+			wp_localize_script(
+				'preview-menu',
+				'PreviewMenu',
+				array(
+					'_nonce' => wp_create_nonce( $this->nonce ),
+				)
+			);
+		}
+	}
+
+	/**
+	 * Store preview menu.
+	 * @since  0.2
+	 */
+	public function ajax() {
+		if (
+			empty( $_POST['_nonce'] )
+			|| empty( $_POST['preview_menu'] )
+			|| ! wp_verify_nonce( $_POST['_nonce'], $this->nonce )
+		) {
+			die;
+		}
+
+		$data = $_POST['preview_menu'];
+
+		if ( empty( $data['items'] ) || empty( $data['menu'] ) ) {
+			die;
+		}
+
+		$data['items'] = $this->parse_post_input( json_decode( stripcslashes( html_entity_decode( $data['items'] ) ), true ) );
+
+		$items = array();
+		foreach ( $data['items'] as $field => $values ) {
+			foreach ( $values as $id => $value ) {
+				if ( empty( $items[ $id ] ) ) {
+					$items[ $id ] = array();
+				}
+				$items[ $id ][ $field ] = $value;
+			}
+		}
+
+		$option = array(
+			'menu'  => $data['menu'],
+			'items' => $items,
+		);
+
+		update_option( $this->option, $option );
+
+		wp_send_json_success( $option );
+	}
+
+	/**
+	 * Convert multilevel post input data to arrays.
+	 * @param  array  $data
+	 * @return array
+	 */
+	public function parse_post_input( $data ) {
+		if ( ! is_array( $data ) ) {
+			return $data;
+		}
+		foreach ( $data as $key => $val ) {
+			if ( is_array( $val ) ) {
+				$val = $this->parse_post_input( $val );
+			}
+			if ( false !== strpos( $key, '[' ) ) {
+				$parts = str_replace( ']', '', $key );
+				$parts = explode( '[', $parts );
+				$parts = array_reverse( $parts );
+				$new   = array();
+				foreach ( $parts as $p ) {
+					if ( empty( $new ) ) {
+						$new = array( $p => $val );
+					} else {
+						$new = array( $p => $new );
+					}
+				}
+				$val = $new;
+			} else {
+				$val = array( $key => $val );
+			}
+			unset( $data[ $key ] );
+			$data = array_replace_recursive( $data, $val );
+		}
+		return $data;
 	}
 
 	/**
@@ -201,41 +444,29 @@ class Preview_Menu
 	 * @since  0.1
 	 */
 	public function meta_box() {
-		global $nav_menu_selected_id;
-		$url = get_bloginfo( 'url' );
-		$url = add_query_arg( 'preview_menu', $nav_menu_selected_id, $url );
+		//global $nav_menu_selected_id;
+		$url = trailingslashit( get_bloginfo( 'url' ) );
+		//$url = add_query_arg( 'preview_menu', $nav_menu_selected_id, $url );
 
 		?>
+		<label for="preview_menu_url"><?php esc_html_e( 'URL', 'preview-menu' ) ?></label>
+		<input type="url" name="preview_menu_url" id="preview_menu_url" class="widefat" value="<?php echo $url; ?>">
 		<label for="preview_menu_location"><?php esc_html_e( 'Select location', 'preview-menu' ) ?></label>
 		<select name="preview_menu_location" id="preview_menu_location" class="widefat">
 			<?php
 			foreach ( $this->menu_locations as $location => $name ) {
 				?>
-			<option value="<?php echo $location; ?>"><?php echo esc_html( $name ) ?></option>
+				<option value="<?php echo $location; ?>"><?php echo esc_html( $name ) ?></option>
 				<?php
 			}
 			?>
 		</select>
 		<p class="button-controls wp-clearfix">
 			<span class="add-to-menu">
-				<a href="<?php echo $url; ?>" id="preview_menu_btn" target="_blank" class="button button-primary"><?php esc_html_e( 'Preview Menu', 'preview-menu' ) ?></a>
+				<button id="preview_menu_btn" class="button button-primary right"><?php esc_html_e( 'Preview Menu', 'preview-menu' ) ?></button>
+				<span class="spinner"></span>
 			</span>
 		</p>
-		<script>
-			jQuery( function( $ ) {
-				$('#preview_menu_btn').on( 'click', function( e ) {
-					e.preventDefault();
-					e.stopPropagation();
-
-					var location = $('#preview_menu_location').val(),
-						url = $(this).attr('href');
-
-					url += '&preview_menu_location=' + location;
-
-					window.open( url, '_blank' );
-				} );
-			} );
-		</script>
 		<?php
 	}
 
